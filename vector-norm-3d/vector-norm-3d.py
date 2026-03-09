426e81ca401e215b77c85416a3b272a4de009ce1
Srijit Mukherjee <151865420+mukherjeesrijit@users.noreply.github.com>
2026-03-09 15:38:40 -0400

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -8,6 +8,4 @@ def vector_norm_3d(v):
     v = np.array(v, dtype = np.float64)
     v_2d = np.atleast_2d(v)
     norms = np.linalg.norm(v_2d, axis = 1)
-    # norms[norms == 0] = 1
-    # print(norms)
     return norms[0] if v.ndim == 1 else norms
