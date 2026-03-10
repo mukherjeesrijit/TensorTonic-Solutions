dbc48111310e65376d06beedb6e3cb8ee556a27f
Srijit Mukherjee <151865420+mukherjeesrijit@users.noreply.github.com>
2026-03-10 18:05:44 -0400

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -8,9 +8,9 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     """
 
     num = np.arange(seq_len)
-    f = lambda i: base ** ((2*np.floor(i/2))/d_model)
+    f = lambda i: (base ** ((2*np.floor(i/2))/d_model))**-1
     den = f(np.arange(d_model))
-    theta = np.outer(num,den**-1)
+    theta = np.outer(num,den)
     PE = np.ones((theta.shape))
     PE[:,0::2] = np.sin(theta[:,::2])
     PE[:,1::2] = np.cos(theta[:,1::2])
