ddbd07e0c9738078b5b8d48a57ca99e8f5a7f0d7
Srijit Mukherjee <151865420+mukherjeesrijit@users.noreply.github.com>
2026-03-11 15:52:50 -0400

    Add solution for percentiles

---
+++ percentiles/percentiles.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def percentiles(x, q):
+    
+    """
+    Compute percentiles using linear interpolation.
+    """
+
+    x,q = map(np.array, [x,q])
+    x = np.sort(x)
+    n = len(x)
+    virtual_id = q*(n-1)/100
+    min_id = np.floor(q*(n-1)/100).astype(int) 
+    min_id = np.clip(min_id, 0, n-2)
+    alpha =  virtual_id - min_id
+    result = (1-alpha)*x[min_id] + (alpha)*x[min_id+1]
+    print(result)
+
+    return result
+    
+    
