3b1e65e43f933dfa0164e79f5ced7485bcd5f069
Srijit Mukherjee <151865420+mukherjeesrijit@users.noreply.github.com>
2026-03-13 12:09:43 -0400

    Update solution for cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -6,9 +6,10 @@ def cross_entropy_loss(y_true, y_pred):
     """
 
     y_true, y_pred = map(np.array, [y_true, y_pred])
+    y_pred = np.clip(y_pred, 1e-15, 1.0)
+    y_pred = -np.log(y_pred)
     n,c = y_pred.shape
     one_hots = np.zeros((n,c))
     one_hots[np.arange(n),y_true] = 1 
     loss_list = np.einsum('ij,ij -> i',y_pred,one_hots)
-    loss_list = - np.log(loss_list)
     return np.mean(loss_list)
