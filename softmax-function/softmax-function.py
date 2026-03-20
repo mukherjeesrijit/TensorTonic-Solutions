5df0f00ee52cedb48e3123d9a753818a4454eed5
Srijit Mukherjee <151865420+mukherjeesrijit@users.noreply.github.com>
2026-03-19 20:29:55 -0400

    Update solution for softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -7,13 +7,16 @@ def softmax(x):
     For 2D, compute row-wise softmax.
     """
 
-    x = np.array(x)
-    if (x.ndim == 1) and (len(x) == 1):
-        return np.array([1])
-    x = np.atleast_2d(x)
-    max = np.max(x, axis = 1, keepdims = True)
-    num = np.exp(x-max)
-    # print(num)
-    den = np.sum(num, axis = 1, keepdims = True)
-    # print(den)
-    return (num/den).squeeze()
+    # note that we are given a batch of vectors
+    x = np.array(x) # transforming list into numpy array
+    x = np.atleast_2d(x) # to make sure for batch 1 has rank 2
+    # print(x.shape)
+    x = x - np.max(x, axis = 1, keepdims = True) # max to reduce axis 1
+    # print(x.shape)
+    num = np.exp(x) # numerator = pointwise exponential for all values
+    # print(num.shape)
+    den = np.sum(num, axis = 1, keepdims = True) # denominator = summation to reduce axis 1
+    # print(den.shape)
+    if x.shape[0] == 1:
+        return (num/den)[0]
+    return (num/den)
